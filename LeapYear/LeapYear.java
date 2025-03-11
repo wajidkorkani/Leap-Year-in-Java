4d21266a79ea8b4d46a0486a87f52d2639a699c6
wajidkorkani <errormrcoder@gmail.com>
2025-03-12 00:16:55 +0500

    Input is being taken and output is being printed.

--- LeapYear/LeapYear.java
+++ LeapYear/LeapYear.java
@@ -1,4 +1,4 @@
-import java.util.scanner;
+import java.util.Scanner;
 
 public class LeapYear{
     public static void main(String[] args){
@@ -8,13 +8,14 @@ public class LeapYear{
     // I don't likle System.out.println 
     // and I don't want to write it again and again 
     // so that is why I wrote this greet function and I call it output easy peasy :)
-    public static void output(String a){
-        System.out.println(value)
+    public static void output(String value){
+        System.out.println(value);
     }
 
     public static void LeapYearFinder(){
-        Scanner scanner = new scanner(System.in);
-        System.out.print("Enter year");
-
+        Scanner scanner = new Scanner(System.in);
+        output("Enter year");
+        int year = scanner.nextInt();
+        output("Working");
     }
 }
