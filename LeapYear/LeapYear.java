b1ec724385222f1888154234f5b0a2c2b5dccc6b
wajidkorkani <errormrcoder@gmail.com>
2025-03-11 23:02:28 +0500

    output function has been created which will be used instead of the System.out.println :)

--- LeapYear/LeapYear.java
+++ LeapYear/LeapYear.java
@@ -5,6 +5,13 @@ public class LeapYear{
         LeapYearFinder();
     }
 
+    // I don't likle System.out.println 
+    // and I don't want to write it again and again 
+    // so that is why I wrote this greet function and I call it output easy peasy :)
+    public static void output(String a){
+        System.out.println(value)
+    }
+
     public static void LeapYearFinder(){
         Scanner scanner = new scanner(System.in);
         System.out.print("Enter year");
