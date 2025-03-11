c8f12ebebdbd4e15264720f4215ed646ee654cf3
wajidkorkani <errormrcoder@gmail.com>
2025-03-11 22:56:05 +0500

    LeapYear.java file has been modified.

---
+++ LeapYear/LeapYear.java
@@ -0,0 +1,13 @@
+import java.util.scanner;
+
+public class LeapYear{
+    public static void main(String[] args){
+        LeapYearFinder();
+    }
+
+    public static void LeapYearFinder(){
+        Scanner scanner = new scanner(System.in);
+        System.out.print("Enter year");
+
+    }
+}
